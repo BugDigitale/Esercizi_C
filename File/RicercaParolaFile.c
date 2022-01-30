a2236ed1f5c9916d11dcc15515765f1997545ba2
Michele D'Ambrosio <94873817+BugDigitale@users.noreply.github.com>
2022-01-30 19:54:53 +0100

    Add files via upload

---
+++ File/RicercaParolaFile.c
@@ -0,0 +1,38 @@
+/*
+30-01-2022
+Dato un file F, si realizzi  un programma che, acquisita una parola P da standard input, 
+ricerchi P all'interno del file dato e stampi sul dispositivo standard di output il numero
+delle occorrenze di P nel file F.
+*/
+
+#include<stdio.h>
+#include<string.h>
+#define max 20
+
+int main(){
+	FILE *f;
+	char p[max], s[max];
+	int k=0;
+	
+	f=fopen("Ricerca.txt", "r");
+	if(f==NULL){
+		printf("C'e' un problema con il file");
+	}else{
+		printf("Inserisci la parola che vuoi cercare: ");
+		scanf("%s", p);
+		fscanf(f, "%s", s);
+		
+		while(!feof(f)){
+			if(strcmp(p, s)==0){
+				k++;
+			}
+			fscanf(f, "%s", s);
+		}
+		if(k>0){
+			printf("\nHo trovato la parola %s e le occorrenze sono %d", p, k);	
+		}else{
+			printf("\nLa parola cercata non si trova nel file");
+		}
+	}
+	fclose(f);
+}
